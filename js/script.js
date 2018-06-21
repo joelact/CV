2392710901601a81a75d1bea3b7c1b3d09c79d81
Joel Teixeira <joelact13@outlook.pt>
2018-06-21 14:34:22 +0100

    added education

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-var BIOGRAPHY_TEXT = "Text";
-var EMAIL = "joelact13@outlook.pt";
+const BIOGRAPHY_TEXT = "Text";
+const EMAIL = "joelact13@outlook.pt";
+const SCHOOLS = ['Agrupamento de Escolas do Castelo da Maia - AECM',
+    'Instituto Superior de Engenharia do Porto'];
+
+const YEARS = []
 
 window.onload = function initialize() {
     try {
@@ -8,4 +12,35 @@ window.onload = function initialize() {
     } catch (err) {
         alert("Illegal Argument");
     }
+    addEducationData();
+}
+
+function addEducationData() {
+    let html = '<ul>';
+
+    SCHOOLS.forEach(element => {
+        if (element.includes('AECM')) {
+            html += `
+            <li class="list">${element}</li>
+            `;
+        } else {
+            html += `
+            <li class="list">${element}<img src="img/ISEP.jpg" id="isep"/>
+            <br/>
+            <ul>
+                <li id="degree">Computer Science Degree</li>
+            </ul>
+            </li>
+        `;
+        }
+    });
+
+    html += '</ul>'
+
+    document.getElementById('schools').innerHTML = html;
+}
+
+
+function lostFocus() {
+
 }
