7a7f3040b984a847eb4b24810d79c25515f5a6ad
Joel Teixeira <joelact13@outlook.pt>
2018-06-23 16:17:25 +0100

    change about me description and add github profile link

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const BIOGRAPHY_TEXT = "Text";
+const age = 20;
+const year_course = 2;
+
+
+const BIOGRAPHY_TEXT = "Hi, my name is Joel Teixeira and I'm " + age + " years old. I always liked computers, that why a choose to study Computer Science, I'm currently in the "
++ year_course + "nd year of the course";
 const EMAIL = "joelact13@outlook.pt";
 const SCHOOLS = ['Agrupamento de Escolas do Castelo da Maia - AECM',
     'Instituto Superior de Engenharia do Porto'];
 
-const YEARS = []
+
+const YEARS = [2016, 2019];
 
 window.onload = function initialize() {
     try {
@@ -28,7 +34,7 @@ function addEducationData() {
             <li class="list">${element}<img src="img/ISEP.jpg" id="isep"/>
             <br/>
             <ul>
-                <li id="degree">Computer Science Degree</li>
+                <li id="degree">Computer Science Degree <span>${YEARS[0]}</span>-<span>${YEARS[1]}</span></li>
             </ul>
             </li>
         `;
