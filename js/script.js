f00b6f6e2d071e18db49617b277696a4bc8f6621
Joel Teixeira <joelact13@outlook.pt>
2018-06-28 00:12:38 +0100

    add hobbies to page

--- js/script.js
+++ js/script.js
@@ -2,29 +2,31 @@ const age = 20;
 const year_course = 2;
 
 
-const BIOGRAPHY_TEXT = "Hi, my name is Joel Teixeira and I'm " + age + " years old. I always liked computers, that why a choose to study Computer Science, I'm currently in the "
-+ year_course + "nd year of the course";
-const EMAIL = "joelact13@outlook.pt";
-const SCHOOLS = ['Agrupamento de Escolas do Castelo da Maia - AECM',
+const biography_text = 'Hi, my name is Joel Teixeira and I\'m ' + age + ' years old. I always liked computers, that why a choose to study Computer Science, I\'m currently in the '
++ year_course + 'nd year of the course.';
+const email = 'joelact13@outlook.pt';
+const schools = ['Agrupamento de Escolas do Castelo da Maia - AECM',
     'Instituto Superior de Engenharia do Porto'];
+const hobbies = ['Soccer/Football', 'Go out with friends', 'Listen to music', 'Playing Computer Games', 'Movies/Series'];
 
 
-const YEARS = [2016, 2019];
+const years = [2016, 2019];
 
 window.onload = function initialize() {
     try {
-        document.getElementById("biography-text").innerHTML = BIOGRAPHY_TEXT;
-        document.getElementById("email").innerHTML = EMAIL;
+        document.getElementById('biography-text').innerHTML = biography_text;
+        document.getElementById('email').innerHTML = email;
     } catch (err) {
-        alert("Illegal Argument");
+        alert('Illegal Argument');
     }
     addEducationData();
-}
+    addHobbies();
+};
 
 function addEducationData() {
     let html = '<ul>';
 
-    SCHOOLS.forEach(element => {
+    schools.forEach(element => {
         if (element.includes('AECM')) {
             html += `
             <li class="list">${element}</li>
@@ -34,19 +36,28 @@ function addEducationData() {
             <li class="list">${element}<img src="img/ISEP.jpg" id="isep"/>
             <br/>
             <ul>
-                <li id="degree">Computer Science Degree <span>${YEARS[0]}</span>-<span>${YEARS[1]}</span></li>
+                <li id="degree">Computer Science Degree <span>${years[0]}</span>-<span>${years[1]}</span></li>
             </ul>
             </li>
         `;
         }
     });
 
-    html += '</ul>'
+    html += '</ul>';
 
     document.getElementById('schools').innerHTML = html;
 }
 
+function addHobbies(){
+    let html = '<ul>';
 
-function lostFocus() {
+    hobbies.forEach(element =>{
+        html += `
+            <li class="list">${element}</li>
+            `;
+    });
 
+    html += '</ul>';
+
+    document.getElementById('listHobbies').innerHTML = html;
 }
